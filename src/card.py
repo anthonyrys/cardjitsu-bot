7a910e731ee4e1836c31e2158e7b4afda9164de1
Anthony Smith <toasteebridges@gmail.com>
2022-04-08 00:25:04 -0400

    added player and card classes

---
+++ src/card.py
@@ -0,0 +1,28 @@
+import random
+from dataclasses import dataclass
+from typing import Dict, List, Union
+
+# global variables
+COLORS: List[str] = ['red', 'blue', 'yellow', 'green', 'orange', 'purple']
+TYPES: List[str] = ['fire', 'water', 'snow']
+
+@dataclass
+class card:
+    # object variables
+    color: str = 'green'
+    type: str = 'ice'
+    rank: int = 5
+    
+    # randomizer
+    def __post_init__(self) -> None:   
+        self.color = random.choice(COLORS)
+        self.type = random.choice(TYPES)
+        self.rank = round(random.random() * 10 + 2)
+
+    # return card info
+    def get(self) -> Dict[str, Union[int, str]]:
+        return({
+            'color': self.color, 
+            'type': self.type,
+            'rank': self.rank
+        })
---
+++ src/player.py
@@ -0,0 +1,80 @@
+import random
+from typing import List
+from card import card
+
+class player:
+    def __init__(self) -> None:
+        # setting up important stuff
+        self.deck: List[card] = []
+        self.deck_amount: int = 15
+        self.hand: List[card] = []
+        self.graveyard: List[card] = []
+        self.selected_card: card = None
+
+        self.create_deck()
+
+    def create_deck(self) -> None:
+        # creates deck of 10 random cards
+        self.deck = [card() for _ in range(self.deck_amount)]
+
+    def invoke_hand(self) -> None:
+        if (len(self.hand) == 3): return
+
+        if (len(self.hand) == 0):
+            # takes first 3 cards from deck
+            self.hand = [self.deck[_] for _ in range(3)]
+            del self.deck[0:3]
+        else:
+            # takes top card from deck
+            self.hand.append(self.deck[0])
+            del self.deck[0]
+
+    def invoke_graveyard(self) -> None:
+        if (self.selected_card == None): return
+        
+        self.graveyard.append(self.selected_card)
+        self.selected_card = None
+
+    def invoke_card(self) -> card:
+        self.invoke_hand()
+
+        # placeholder /s
+        for c, _ in enumerate(self.hand, start = 1):
+            print(f'\ncard {c}:')
+            for i, v in enumerate(_.get().values()):
+                print(v)
+
+        while True:
+            try:
+                select: int = int(input('\npick a card: ')) - 1
+
+                self.selected_card = self.hand[select]
+                del self.hand[select]
+                break
+ 
+            except IndexError:
+                continue
+
+            except ValueError:
+                continue
+        # placeholder /e
+
+        return (self.selected_card)
+
+    def get_hand(self) -> List[card]:
+        return (self.hand)
+
+class bot(player):
+    def __init__(self) -> None:
+        # calling to player class
+        super().__init__()
+
+    def invoke_card(self) -> None:
+        self.invoke_hand()
+
+        # gets random card from hand
+        item: card = random.choice(self.hand)
+        self.selected_card = item
+        self.hand.remove(item)
+
+        return (self.selected_card)
